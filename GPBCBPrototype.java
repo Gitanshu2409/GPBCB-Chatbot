a15aff76defed283f26a1a7c972725157017cc8e
Gitanshu Pal <86179904+Gitanshu2409@users.noreply.github.com>
2022-11-18 17:20:16 +0530

    Add files via upload

---
+++ GPBCBPrototype.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class GPBCBPrototype {
+  public static void main(String[]args){
+// Defining Scanner to read input.
+    Scanner scnr;
+    scnr = new Scanner(System.in);
+
+// Welcome Program
+    System.out.println("|*************************************************|");
+    System.out.println("|***|Welcome to GPBCB, I am Still a Prototype.|***|");
+    System.out.println("|*************************************************|");
+    System.out.println("#Please enter your name, this will make it easier for me to talk to you.#");
+    String usrname = scnr.nextLine();
+    System.out.println("#Oh i see :)#");
+    System.out.println("Nice to meet you : "+usrname);
+//Paused
+
+  }
+}
